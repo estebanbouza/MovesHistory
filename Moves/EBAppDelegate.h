e0bf2f13f1cff6deb521b2ed843d5897d67daeb1
Esteban Bouza <esteban.bouza@mobgen.com>
2013-10-13 23:44:01 +0200

    Initial commit

---
+++ Moves/EBAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  EBAppDelegate.h
+//  Moves
+//
+//  Created by Esteban on 10/13/13.
+//  Copyright (c) 2013 Esteban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
